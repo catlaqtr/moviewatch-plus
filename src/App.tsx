172c9f9baae830a06c45bfc56ac4f3738f411662
meteunal <meteunal46@hotmail.com>
2025-04-26 22:32:21 -0400

    fetched

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import { MovieProvider } from "./features/context/MovieProvider";
 import { useMoviesContext } from "./features/context/useMoviesContext";
 
 function AppContent() {
   const { state, dispatch } = useMoviesContext();
   const { query } = state;
+  useEffect(() => {
+    if (query.length < 3) return;
+
+    dispatch({ type: "startSearching" });
+
+    fetch(`https://www.omdbapi.com/?apikey=5df2db86&s=${query}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.Response === "False") {
+          dispatch({ type: "searchError", payload: "Movie not found" });
+        } else {
+          dispatch({ type: "searchSuccess", payload: data.Search });
+        }
+      })
+      .catch((err) => dispatch({ type: "searchError", payload: err.message }));
+  }, [query]);
 
   return (
     <>
